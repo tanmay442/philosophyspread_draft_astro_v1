508ee04bdbaab29dce44a540d213a5341d24a341
tanmay442 <goeltanmay442@gmail.com>
2026-04-04 17:04:37 +0530

    OK so added the clerk integration and some pagination issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
+import clerk from '@clerk/astro';
 
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), clerk()],
+  adapter: node({ mode: 'standalone' }),
+  output: 'server',
   server: {
     host: '0.0.0.0',
     port: 3000,
   },
 });
+
