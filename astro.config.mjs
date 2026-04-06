2b12fae155e8b391e15a149ea9ff1f3e00ddd760
tanmay442 <goeltanmay442@gmail.com>
2026-04-06 23:40:09 +0530

    Revert "cloudflare hosting pre prep"
    This reverts commit 8468d1c2e0bdb3ecab52910a1aa638be0d92b5df.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
 import clerk from '@clerk/astro';
 
 export default defineConfig({
   integrations: [tailwind(), mdx(), clerk()],
-  adapter: cloudflare(),
+  adapter: node({ mode: 'standalone' }),
   output: 'server',
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
 });
+
